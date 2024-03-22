172d47b345e145617c68336369ea115e61b8243a
sushil-arya <sushilarya888@gmail.com>
2024-03-22 12:47:30 +0530

    Final-code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './pages/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import something from somelocation/somelibrary
+
+//import someNamedImport from someLocation/someLibrary
+import { Login } from './pages/Login';
+
+import { Register } from './pages/Register';
+import { BrowserRouter as GrandParent, Routes as Parent, Route as Child } from 'react-router-dom';
+import PageNotFound from './pages/PageNotFound';
+
+export default function App() {
+    //Every function return something
+    return (
+            <>  
+                <GrandParent>
+                    <Parent>
+                        <Child path="/" element={<Home />} >
+
+                        </Child>
+                        <Child path="/login" element={<Login />} >
+
+                        </Child>
+                        <Child path="/register" element={<Register />} >
+
+                        </Child>
+                        <Child path="*" element={<PageNotFound />} >
+
+                        </Child>
+                    </Parent>
+                </GrandParent>
+            </>
+    );
 }
-
-export default App;
---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class PageNotFound extends Component {
+    render() {
+        return (
+                    <>
+                            PageNotFound
+
+                            <br />
+
+                            <a href="/">Go to Home Page</a>
+                    </>
+        )
+    }
+}
